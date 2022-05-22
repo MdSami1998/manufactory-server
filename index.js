36e8b17da2621b273b2bee02b1cc582fac57f167
Md. Sami Al Zaber <samizaber822@gmail.com>
2022-05-22 16:14:27 +0600

    get api created for tools

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
+const cors = require("cors");
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
@@ -8,13 +10,34 @@ const app = express();
 
 // middleware 
 app.use(express.json());
-app.use(cors()),
+app.use(cors());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h43xn.mongodb.net/?retryWrites=true&w=majority`;
 
-    app.get('/', (req, res) => {
-        res.send('I am running')
-    });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        await client.connect();
+        const toolsCollection = client.db("manufactory").collection("tools");
+
+        // tools get api
+
+        app.get('/tools', async (req, res) => {
+            const query = {};
+            const cursor = toolsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('I am running')
+});
 app.listen(port, () => {
     console.log('Listening to port,', port)
 })
