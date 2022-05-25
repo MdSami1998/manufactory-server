d24b932560939c191b1dae1a4dee77996914432f
Md. Sami Al Zaber <samizaber822@gmail.com>
2022-05-25 12:17:48 +0600

    api created for delete order from myOrder page

--- index.js
+++ index.js
@@ -53,6 +53,14 @@ async function run() {
             const orders = await cursor.toArray();
             res.send(orders);
         })
+
+        // api for delete single order from my order
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            res.send(result);
+        })
     } finally {
 
     }
